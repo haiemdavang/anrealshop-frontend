47c86baedd4669bdf451e4619b7cd062974f759a
haiemdavang <lengochai7304@gmail.com>
2026-02-14 03:27:10 +0700

    optimize fetch product list

--- src/components/User/Chatbox/ChatBtn.tsx
+++ src/components/User/Chatbox/ChatBtn.tsx
@@ -14,7 +14,9 @@ const ChatBtn = () => {
     return (
         <>
             {/* Chatbox */}
-            <ChatboxPane isOpen={isChatboxOpen} onClose={() => setIsChatboxOpen(false)} />
+            {isChatboxOpen && (
+                <ChatboxPane isOpen={isChatboxOpen} onClose={() => setIsChatboxOpen(false)} />
+            )}
 
             {/* Floating Chat Button */}
             {!isChatboxOpen && (
